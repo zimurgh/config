8169d5253e86793a021be57d754f22bc39b6adfc
mcarpenter <mcarpenter.dev@gmail.com>
2026-07-18 16:09:36 -0400

    Add raven pub key

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,8 @@
     shell = pkgs.nushell;
     packages = with pkgs; [];
     openssh.authorizedKeys.keys = [
-      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG7b1bSa+7/SDBOUmXmwO703/R6V89B4D04w/uv1FOsA mcarpenter.dev@gmail.com"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIGHbhvT5z9IBhZm+gFDbEpLFVjyBxgE2NGvyrOHNlILI mcarpenter.dev@gmail.com" # raven
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIG7b1bSa+7/SDBOUmXmwO703/R6V89B4D04w/uv1FOsA mcarpenter.dev@gmail.com" # calcifer
     ];
   };
 
